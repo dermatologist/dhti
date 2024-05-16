0b8bd27b0ab7187f27b9a5aed1faf76312e11061
dermatologist <github@gulfdoctor.net>
2024-05-16 17:51:37 -0400

    chore: Update docker-compose.yml to include langfuse service and environment variables

--- genai/app/server.py
+++ genai/app/server.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from langserve import add_routes
-
+from langchain_core.runnables.config import RunnableConfig
+from langfuse import Langfuse
+from langfuse.callback import CallbackHandler
 from pirate_speak.chain import chain as pirate_speak_chain
+langfuse_handler = CallbackHandler()
+
+# Tests the SDK connection with the server
+langfuse_handler.auth_check()
+config = RunnableConfig(callbacks=[langfuse_handler])
 
 app = FastAPI(title="LangServe Launch Example")
 
-add_routes(app, pirate_speak_chain, path="/pirate-speak")
+add_routes(app, pirate_speak_chain.with_config(config), path="/pirate-speak")
 
 if __name__ == "__main__":
     import uvicorn
