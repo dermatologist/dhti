0e3d77d54fd1f3d85e0b28bb93452800a7ad6ffa
dermatologist <github@gulfdoctor.net>
2024-06-06 07:57:06 -0500

    chore: Add fhire language support to langserve

--- genai/app/server.py
+++ genai/app/server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from langserve import add_routes
 from langchain_core.runnables.config import RunnableConfig
 from pirate_speak.chain import chain as pirate_speak_chain
+from dhti_elixir_fhire import chain as fhire_chain
 
 app = FastAPI(title="LangServe Launch Example")
 
@@ -12,8 +13,10 @@ try:
     langfuse_handler = CallbackHandler()
     config = RunnableConfig(callbacks=[langfuse_handler])
     add_routes(app, pirate_speak_chain.with_config(config), path="/pirate-speak")
+    add_routes(app, fhire_chain.with_config(config), path="/fhire")
 except:
     add_routes(app, pirate_speak_chain, path="/pirate-speak")
+    add_routes(app, fhire_chain, path="/fhire")
 
 if __name__ == "__main__":
     import uvicorn
