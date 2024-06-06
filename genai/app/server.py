912edd6242f58ed94ffdaa5203eddd91641ffd85
dermatologist <github@gulfdoctor.net>
2024-06-06 10:19:31 -0500

    chore: Update Dockerfile to use poetry for installing dependencies and remove unnecessary commands

--- genai/app/server.py
+++ genai/app/server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from langserve import add_routes
 from langchain_core.runnables.config import RunnableConfig
-from pirate_speak.chain import chain as pirate_speak_chain
 from dhti_elixir_fhire import chain as fhire_chain
 import uvicorn
 
@@ -16,10 +15,8 @@ try:
     langfuse_handler.auth_check()
     langfuse_handler = CallbackHandler()
     config = RunnableConfig(callbacks=[langfuse_handler])
-    add_routes(app, pirate_speak_chain.with_config(config), path="/pirate-speak")
     add_routes(app, fhire_chain.with_config(config), path="/fhire")
 except:
-    add_routes(app, pirate_speak_chain, path="/pirate-speak")
     add_routes(app, fhire_chain, path="/fhire")
 
 if __name__ == "__main__":
