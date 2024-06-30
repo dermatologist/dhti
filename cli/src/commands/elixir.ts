9ef161b29fe8a1ed1ec21d0cce31daf7bc59d301
Bell Eapen <github_public@gulfdoctor.net>
2024-06-30 23:39:22 +0000

    chore: Update Elixir command to support installation from a specific Github repository and branch

--- cli/src/commands/elixir.ts
+++ cli/src/commands/elixir.ts
@@ -2,29 +2,22 @@ import {Args, Command, Flags} from '@oclif/core'
 
 export default class Elixir extends Command {
   static override args = {
-    file: Args.string({description: 'file to read'}),
+    op: Args.string({description: 'Operation to perform (install or uninstall)'}),
   }
 
-  static override description = 'describe the command here'
+  static override description = 'Install or uninstall elixirs and create a Docker image'
 
   static override examples = [
     '<%= config.bin %> <%= command.id %>',
   ]
 
   static override flags = {
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
+    git: Flags.string({char: 'g', description: 'Github repository to install'}),
+    branch: Flags.string({char: 'b', description: 'Branch to install from'}),
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Elixir)
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /home/M267492/repos/dhti/cli/src/commands/elixir.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
   }
 }
