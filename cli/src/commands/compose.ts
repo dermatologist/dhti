490eb968337a91aabbd866d954c0c650d8c9f42f
Bell Eapen <github_public@gulfdoctor.net>
2024-06-30 12:55:54 +0000

    chore: Add volumes for Redis and Neo4j data in docker-compose.yml

--- cli/src/commands/compose.ts
+++ cli/src/commands/compose.ts
@@ -1,30 +1,34 @@
 import {Args, Command, Flags} from '@oclif/core'
 
+import fs from 'fs'
+
 export default class Compose extends Command {
   static override args = {
-    file: Args.string({description: 'file to read'}),
+    op: Args.string({description: 'Operation to perform (add or delete)'}),
   }
 
-  static override description = 'describe the command here'
+  static override description = 'Generates a docker-compose.yml file from a list of modules'
 
   static override examples = [
     '<%= config.bin %> <%= command.id %>',
   ]
 
   static override flags = {
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
     // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
+    module: Flags.string({char: 'm', multiple: true, description: 'Modules to add from ( default, gateway, langserve, openmrs, ollama, langfuse, fhir, redis, cql and neo4j)'}),
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Compose)
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /home/M267492/repos/dhti/cli/src/commands/compose.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    // console.log('args', args) //args { op: 'add' }
+    // console.log('flags', flags) //flags { module: [ 'default', 'langserve', 'redis' ] }
+
+    try {
+      const data = fs.readFileSync('docker-compose.yml', 'utf8');
+      console.log(data);
+    } catch (err) {
+      console.error(err);
     }
   }
 }
