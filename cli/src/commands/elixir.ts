ac524f9733d2bc3bda475b469b4567293531b720
Bell Eapen <github_public@gulfdoctor.net>
2024-07-01 02:02:43 +0000

    chore: Update Elixir command to include lazy loading for images

--- cli/src/commands/elixir.ts
+++ cli/src/commands/elixir.ts
@@ -1,4 +1,6 @@
 import {Args, Command, Flags} from '@oclif/core'
+import { directory } from '@oclif/core/args'
+import { dir } from 'console'
 import fs from 'fs'
 import request from 'request'
 export default class Elixir extends Command {
@@ -17,10 +19,17 @@ export default class Elixir extends Command {
     branch: Flags.string({char: 'b', description: 'Branch to install from', default: "develop"}),
     name: Flags.string({char: 'n', description: 'Name of the elixir'}),
     type: Flags.string({char: 't', description: 'Type of elixir (chain, tool or agent)', default: "chain"}),
+    workdir: Flags.string({char: 'w', description: 'Working directory to install the elixir', default: "/tmp/elixir"}),
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Elixir)
+
+    // Create a directory to install the elixir
+    if (!fs.existsSync(flags.workdir)){
+      fs.mkdirSync(flags.workdir);
+    }
+    fs.cpSync('src/resources/genai', flags.workdir, {recursive: true})
 
     // get bootstrap.py file content
     const url = `${flags.git}/blob/${flags.branch}/tests/bootstrap.py`.replace('.git', '')
@@ -28,7 +37,7 @@ export default class Elixir extends Command {
     if (!error && response.statusCode == 200) {
         var to_add = body.split('#DHTI_ADD')[1];
         // Continue with your processing here.
-        fs.readFileSync('src/resources/genai/app/bootstrap.py', 'utf8').replace('#DHTI_ADD', `#DHTI_ADD\nAdded by (Edit if needed) ${flags.name}\n${to_add}`)
+        fs.readFileSync(`${flags.workdir}/app/bootstrap.py`, 'utf8').replace('#DHTI_ADD', `#DHTI_ADD\nAdded by (Edit if needed) ${flags.name}\n${to_add}`)
     }else{
         console.log("Error: ", error)
         console.log("Status code: ", response.statusCode)
@@ -36,27 +45,27 @@ export default class Elixir extends Command {
     }
     });
 
-    const pyproject = fs.readFileSync('src/resources/genai/pyproject.toml', 'utf8')
-    const original_server = fs.readFileSync('src/resources/genai/app/server.py', 'utf8')
+    const pyproject = fs.readFileSync(`${flags.workdir}/pyproject.toml`, 'utf8')
+    const original_server = fs.readFileSync(`${flags.workdir}/app/server.py`, 'utf8')
     const line_to_add = `${flags.name} = { git = "${flags.git}", branch = "${flags.branch}" }`
     const new_pyproject = pyproject.replace('[tool.poetry.dependencies]', `[tool.poetry.dependencies]\n${line_to_add}`)
     const cli_import = `from ${flags.name} import ${flags.type} as ${flags.name}_${flags.type}\n`
-    const new_cli_import =  fs.readFileSync('src/resources/genai/app/server.py', 'utf8').replace('#DHTI_CLI_IMPORT', `#DHTI_CLI_IMPORT\n${cli_import}`)
+    const new_cli_import =  fs.readFileSync(`${flags.workdir}/app/server.py`, 'utf8').replace('#DHTI_CLI_IMPORT', `#DHTI_CLI_IMPORT\n${cli_import}`)
     const langfuse_route = `add_routes(app, ${flags.name}_${flags.type}.with_config(config), path="/${flags.name}")`
     const new_langfuse_route = new_cli_import.replace('#DHTI_LANGFUSE_ROUTE', `#DHTI_LANGFUSE_ROUTE\n    ${langfuse_route}`)
     const normal_route = `add_routes(app, ${flags.name}_${flags.type}, path="/${flags.name}")`
     const final_route = new_langfuse_route.replace('#DHTI_NORMAL_ROUTE', `#DHTI_NORMAL_ROUTE\n    ${normal_route}`)
     // if args.op === install, add the line to the pyproject.toml file
     if (args.op === 'install') {
-      fs.writeFileSync('src/resources/genai/pyproject.toml', new_pyproject)
-      fs.writeFileSync('src/resources/genai/app/server.py', final_route)
+      fs.writeFileSync(`${flags.workdir}/pyproject.toml`, new_pyproject)
+      fs.writeFileSync(`${flags.workdir}/app/server.py`, final_route)
     } else {
       // if args.op === uninstall, remove the line from the pyproject.toml file
-      fs.writeFileSync('src/resources/genai/pyproject.toml', pyproject.replace(line_to_add, ''))
+      fs.writeFileSync(`${flags.workdir}/pyproject.toml`, pyproject.replace(line_to_add, ''))
       let new_server =  original_server.replace(cli_import, '')
       new_server = new_server.replace(langfuse_route, '')
       new_server = new_server.replace(normal_route, '')
-      fs.writeFileSync('src/resources/genai/app/server.py', new_server)
+      fs.writeFileSync(`${flags.workdir}/app/server.py`, new_server)
     }
 
   }
