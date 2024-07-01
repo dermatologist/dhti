33f49000ab07dc84d9fa891610ff6120059aa0b4
Bell Eapen <github_public@gulfdoctor.net>
2024-07-01 15:16:09 +0000

    chore: Update Conch command to use version 1.0.0 of the conch

--- cli/src/commands/conch.ts
+++ cli/src/commands/conch.ts
@@ -17,7 +17,7 @@ export default class Conch extends Command {
     git: Flags.string({char: 'g', description: 'Github repository to install', default: "none"}),
     branch: Flags.string({char: 'b', description: 'Branch to install from', default: "develop"}),
     name: Flags.string({char: 'n', description: 'Name of the elixir'}),
-    repo_version: Flags.string({char: 'v', description: 'Version of the conch', default: "0.1.0"}),
+    repo_version: Flags.string({char: 'v', description: 'Version of the conch', default: "1.0.0"}),
     workdir: Flags.string({char: 'w', description: 'Working directory to install the conch', default: "/tmp/conch"}),
   }
 
@@ -35,15 +35,40 @@ export default class Conch extends Command {
     }
     fs.cpSync('src/resources/spa', flags.workdir, {recursive: true})
 
-    // git clone the repository
-    exec(`git clone ${flags.git} ${flags.workdir}/${flags.name}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    });
+    // // git clone the repository
+    // exec(`git clone ${flags.git} ${flags.workdir}/${flags.name}`, (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`exec error: ${error}`);
+    //     return;
+    //   }
+    //   console.log(`stdout: ${stdout}`);
+    //   console.error(`stderr: ${stderr}`);
+    // });
+
+    // // Checkout the branch
+    // exec(`cd ${flags.workdir}/${flags.name} && git checkout ${flags.branch}`, (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`exec error: ${error}`);
+    //     return;
+    //   }
+    //   console.log(`stdout: ${stdout}`);
+    //   console.error(`stderr: ${stderr}`);
+    // });
+
+    // Read and process importmap.json
+    let importmap = JSON.parse(fs.readFileSync(`${flags.workdir}/def/importmap.json`, 'utf8'));
+    importmap.imports[flags.name.replace('openmrs-', '@openmrs/')] = `./${flags.name}-${flags.repo_version}/${flags.name}.js`;
+    fs.writeFileSync(`${flags.workdir}/def/importmap.json`, JSON.stringify(importmap, null, 2));
+
+    // Read and process spa-assemble-config.json
+    let spa_assemble_config = JSON.parse(fs.readFileSync(`${flags.workdir}/def/spa-assemble-config.json`, 'utf8'));
+    spa_assemble_config.frontendModules[flags.name.replace('openmrs-', '@openmrs/')] = `${flags.repo_version}`;
+    fs.writeFileSync(`${flags.workdir}/def/spa-assemble-config.json`, JSON.stringify(spa_assemble_config, null, 2));
+
+     // Read and process Dockerfile
+    let dockerfile = fs.readFileSync(`${flags.workdir}/Dockerfile`, 'utf8');
+    dockerfile = dockerfile.replaceAll('conch', flags.name).replaceAll('version', flags.repo_version);
+    fs.writeFileSync(`${flags.workdir}/Dockerfile`, dockerfile);
 
   }
 }
