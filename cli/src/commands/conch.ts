0a6d9e29f19feb27fbd725d1081ffb49aeb81f01
Bell Eapen <github_public@gulfdoctor.net>
2024-07-01 14:30:15 +0000

    chore: Update Conch command to include installation of conchs and create a Docker image

--- cli/src/commands/conch.ts
+++ cli/src/commands/conch.ts
@@ -1,6 +1,7 @@
 import {Args, Command, Flags} from '@oclif/core'
 import fs from 'fs'
 import request from 'request'
+import { exec } from 'child_process';
 export default class Conch extends Command {
   static override args = {
         op: Args.string({description: 'Operation to perform (install or uninstall)'}),
@@ -23,13 +24,26 @@ export default class Conch extends Command {
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Conch)
 
+    if(!flags.name){
+      console.log("Please provide a name for the conch")
+      this.exit(1)
+    }
+
     // Create a directory to install the elixir
     if (!fs.existsSync(flags.workdir)){
       fs.mkdirSync(flags.workdir);
     }
-    fs.cpSync('src/resources/genai', flags.workdir, {recursive: true})
+    fs.cpSync('src/resources/spa', flags.workdir, {recursive: true})
 
+    // git clone the repository
+    exec(`git clone ${flags.git} ${flags.workdir}/${flags.name}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
 
-    
   }
 }
