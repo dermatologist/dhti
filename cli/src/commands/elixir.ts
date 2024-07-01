b73f97e909ebddb9184fc8534b67a905aa582602
Bell Eapen <github_public@gulfdoctor.net>
2024-07-01 01:32:22 +0000

    chore: Refactor Elixir command to improve code organization and readability

--- cli/src/commands/elixir.ts
+++ cli/src/commands/elixir.ts
@@ -22,26 +22,26 @@ export default class Elixir extends Command {
     const {args, flags} = await this.parse(Elixir)
 
     const pyproject = fs.readFileSync('src/resources/genai/pyproject.toml', 'utf8')
+    const original_server = fs.readFileSync('src/resources/genai/app/server.py', 'utf8')
     const line_to_add = `${flags.name} = { git = "${flags.git}", branch = "${flags.branch}" }`
     const new_pyproject = pyproject.replace('[tool.poetry.dependencies]', `[tool.poetry.dependencies]\n${line_to_add}`)
     const cli_import = `from ${flags.name} import ${flags.type} as ${flags.name}_${flags.type}\n`
     const new_cli_import =  fs.readFileSync('src/resources/genai/app/server.py', 'utf8').replace('#DHTI_CLI_IMPORT', `#DHTI_CLI_IMPORT\n${cli_import}`)
     const langfuse_route = `add_routes(app, ${flags.name}_${flags.type}.with_config(config), path="/${flags.name}")`
-    const new_langfuse_route = fs.readFileSync('src/resources/genai/app/server.py', 'utf8').replace('#DHTI_LANGFUSE_ROUTE', `#DHTI_LANGFUSE_ROUTE\n\t${langfuse_route}`)
+    const new_langfuse_route = new_cli_import.replace('#DHTI_LANGFUSE_ROUTE', `#DHTI_LANGFUSE_ROUTE\n    ${langfuse_route}`)
     const normal_route = `add_routes(app, ${flags.name}_${flags.type}, path="/${flags.name}")`
-    const new_normal_route = fs.readFileSync('src/resources/genai/app/server.py', 'utf8').replace('#DHTI_NORMAL_ROUTE', `#DHTI_NORMAL_ROUTE\n\t${normal_route}`)
+    const final_route = new_langfuse_route.replace('#DHTI_NORMAL_ROUTE', `#DHTI_NORMAL_ROUTE\n    ${normal_route}`)
     // if args.op === install, add the line to the pyproject.toml file
     if (args.op === 'install') {
       fs.writeFileSync('src/resources/genai/pyproject.toml', new_pyproject)
-      fs.writeFileSync('src/resources/genai/app/server.py', new_cli_import)
-      fs.writeFileSync('src/resources/genai/app/server.py', new_langfuse_route)
-      fs.writeFileSync('src/resources/genai/app/server.py', new_normal_route)
+      fs.writeFileSync('src/resources/genai/app/server.py', final_route)
     } else {
       // if args.op === uninstall, remove the line from the pyproject.toml file
       fs.writeFileSync('src/resources/genai/pyproject.toml', pyproject.replace(line_to_add, ''))
-      fs.writeFileSync('src/resources/genai/app/server.py', new_cli_import.replace(cli_import, ''))
-      fs.writeFileSync('src/resources/genai/app/server.py', new_langfuse_route.replace(langfuse_route, ''))
-      fs.writeFileSync('src/resources/genai/app/server.py', new_normal_route.replace(normal_route, ''))
+      let new_server =  original_server.replace(cli_import, '')
+      new_server = new_server.replace(langfuse_route, '')
+      new_server = new_server.replace(normal_route, '')
+      fs.writeFileSync('src/resources/genai/app/server.py', new_server)
     }
 
   }
