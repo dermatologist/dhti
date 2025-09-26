c11481e7e475cc4e9ff051780c71ea0c8f4c6d45
dermatologist <github@gulfdoctor.net>
2025-09-25 20:31:41 -0500

    Remove branch specification from conch install command in demo.sh for clarity

--- demo.sh
+++ demo.sh
@@ -4,6 +4,6 @@ npm link
 dhti-cli compose add -m openmrs -m langserve
 dhti-cli elixir install -g https://github.com/dermatologist/dhti-elixir-template.git -n dhti-elixir-template
 dhti-cli docker -n beapen/genai-test:1.0 -t elixir
-dhti-cli conch install -g https://github.com/dermatologist/openmrs-esm-dhti-template.git -n openmrs-esm-dhti-template -b feature/cds-hook-2
+dhti-cli conch install -g https://github.com/dermatologist/openmrs-esm-dhti-template.git -n openmrs-esm-dhti-template
 dhti-cli docker -n beapen/conch-test:1.0 -t conch
 dhti-cli docker -u
