cd735097cb8afe36c109e7b1c8e74016f690912a
Bell Eapen <github@gulfdoctor.net>
2025-09-26 08:10:22 -0500

    Feature/fix path 1 (#71)
    * refactor: improve code structure and readability in compose, conch, and elixir commands
    
    * feat: add typedoc configuration file

--- src/commands/compose.ts
+++ src/commands/compose.ts
@@ -2,6 +2,8 @@ import {Args, Command, Flags} from '@oclif/core'
 import yaml from 'js-yaml'
 import fs from 'node:fs'
 import os from 'node:os'
+import path from 'node:path'
+import {fileURLToPath} from 'node:url'
 
 export default class Compose extends Command {
   static override args = {
@@ -10,30 +12,41 @@ export default class Compose extends Command {
 
   static override description = 'Generates a docker-compose.yml file from a list of modules'
 
-  static override examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
+  static override examples = ['<%= config.bin %> <%= command.id %>']
 
   static override flags = {
-    file: Flags.string({char: 'f', default: `${os.homedir()}/dhti/docker-compose.yml`, description: 'Full path to the docker compose file to read from. Creates if it does not exist'}),
+    file: Flags.string({
+      char: 'f',
+      default: `${os.homedir()}/dhti/docker-compose.yml`,
+      description: 'Full path to the docker compose file to read from. Creates if it does not exist',
+    }),
     // flag with a value (-n, --name=VALUE)
-    module: Flags.string({char: 'm', description: 'Modules to add from ( langserve, openmrs, ollama, langfuse, cqlFhir, redis, neo4j and mcpFhir)', multiple: true}),
+    module: Flags.string({
+      char: 'm',
+      description: 'Modules to add from ( langserve, openmrs, ollama, langfuse, cqlFhir, redis, neo4j and mcpFhir)',
+      multiple: true,
+    }),
   }
 
   public static init = () => {
     // Create ${os.homedir()}/dhti if it does not exist
     if (!fs.existsSync(`${os.homedir()}/dhti`)) {
-      fs.mkdirSync(`${os.homedir()}/dhti`);
+      fs.mkdirSync(`${os.homedir()}/dhti`)
     }
 
     // Create ${os.homedir()}/dhti/docker-compose.yml if it does not exist
     if (!fs.existsSync(`${os.homedir()}/dhti/docker-compose.yml`)) {
-      fs.writeFileSync(`${os.homedir()}/dhti/docker-compose.yml`, '', 'utf8');
+      fs.writeFileSync(`${os.homedir()}/dhti/docker-compose.yml`, '', 'utf8')
     }
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Compose)
+
+    // Resolve resources directory for both dev (src) and packaged (dist)
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const RESOURCES_DIR = path.resolve(__dirname, '../resources')
 
     // console.log('args', args) //args { op: 'add' }
     // console.log('flags', flags) //flags { module: [ 'default', 'langserve', 'redis' ] }
@@ -50,7 +63,6 @@ export default class Compose extends Command {
     const fhir = ['fhir', 'postgres-db']
     const mcpFhir = ['mcp-fhir', 'fhir', 'postgres-db']
 
-
     const _modules: {[key: string]: string[]} = {
       cqlFhir,
       fhir,
@@ -62,42 +74,41 @@ export default class Compose extends Command {
       ollama,
       openmrs,
       redis,
-      webui
+      webui,
     }
 
     try {
-
-      const masterData: any = yaml.load(fs.readFileSync('src/resources/docker-compose-master.yml', 'utf8'));
-      let existingData:any = {services: {}, version: '3.8'};
+      const masterData: any = yaml.load(fs.readFileSync(path.join(RESOURCES_DIR, 'docker-compose-master.yml'), 'utf8'))
+      let existingData: any = {services: {}, version: '3.8'}
       if (fs.existsSync(flags.file)) {
-          existingData = yaml.load(fs.readFileSync(flags.file, 'utf8'));
+        existingData = yaml.load(fs.readFileSync(flags.file, 'utf8'))
       } else {
-        Compose.init(); // Create the file if it does not exist
+        Compose.init() // Create the file if it does not exist
       }
 
       // Echo the existing data to the console
       if (args.op === 'read') {
-        console.log(existingData);
-        return;
+        console.log(existingData)
+        return
       }
 
       // Delete flags.file if args.op is reset
       if (args.op === 'reset') {
-        fs.unlinkSync(flags.file);
-        Compose.init(); // Recreate the file
+        fs.unlinkSync(flags.file)
+        Compose.init() // Recreate the file
       }
 
       // if existing data is not null and arg is delete, remove the modules from the existing data
       if (Object.keys(existingData.services).length > 0 && args.op === 'delete') {
         // Expand the modules to remove using _modules object
-        let modulesToDelete: any[] = [];
+        let modulesToDelete: any[] = []
         for (const module of flags.module ?? []) {
-          modulesToDelete = modulesToDelete.concat(_modules[module]);
+          modulesToDelete = modulesToDelete.concat(_modules[module])
         }
 
         for (const module of modulesToDelete ?? []) {
           if (existingData.services[module]) {
-            delete existingData.services[module];
+            delete existingData.services[module]
           }
         }
       }
@@ -105,30 +116,29 @@ export default class Compose extends Command {
       // if arg is add, add the modules from the master data
       if (args.op === 'add') {
         // Expand the modules to add using _modules object
-        let modulesToAdd: any[] = [];
+        let modulesToAdd: any[] = []
         for (const module of flags.module ?? []) {
-          modulesToAdd = modulesToAdd.concat(_modules[module]);
+          modulesToAdd = modulesToAdd.concat(_modules[module])
         }
 
         for (const module of modulesToAdd ?? []) {
-          existingData.services[module] = masterData.services[module];
+          existingData.services[module] = masterData.services[module]
         }
       }
 
       // Add all volumes from master data to existing data by default
-      existingData.volumes = {};
+      existingData.volumes = {}
       for (const key of Object.keys(masterData.volumes)) {
-        existingData.volumes[key] = masterData.volumes[key];
+        existingData.volumes[key] = masterData.volumes[key]
       }
 
-      const toWrite = yaml.dump(existingData).replaceAll('null', '');
+      const toWrite = yaml.dump(existingData).replaceAll('null', '')
 
-      console.log('Writing file:', toWrite);
+      console.log('Writing file:', toWrite)
 
-      fs.writeFileSync(flags.file, toWrite, 'utf8');
-
+      fs.writeFileSync(flags.file, toWrite, 'utf8')
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   }
 }
