dab9db8aab9e9127e8628980d66db4e42f470ff0
Bell Eapen <github_public@gulfdoctor.net>
2025-11-10 10:00:20 -0600

    Feature/copy bootstrap (#86)
    * refactor: improve code readability and structure in Docker command
    
    * docs: add instructions to copy updated file and restart services
    
    * fix: correct minor-wording and enhance patch-wording in version bump workflow
    
    * fix: update container description and improve error handling in Docker command

--- src/commands/docker.ts
+++ src/commands/docker.ts
@@ -11,78 +11,121 @@ export default class Docker extends Command {
 
   static override description = 'Build a docker project and update docker-compose file'
 
-  static override examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
+  static override examples = ['<%= config.bin %> <%= command.id %>']
 
   static override flags = {
     down: Flags.boolean({char: 'd', default: false, description: 'Run docker-compose down after building'}),
-    file: Flags.string({char: 'f', default: `${os.homedir()}/dhti/docker-compose.yml`, description: 'Full path to the docker compose file to edit or run.'}),
+    file: Flags.string({
+      char: 'f',
+      default: `${os.homedir()}/dhti/docker-compose.yml`,
+      description: 'Full path to the docker compose file to edit or run.',
+    }),
+    container: Flags.string({
+      char: 'c',
+      default: 'dhti-langserve-1',
+      description: 'Name of the container to copy the bootstrap file to while in dev mode',
+    }),
     name: Flags.string({char: 'n', description: 'Name of the container to build'}),
-    type : Flags.string({char: 't', default: 'elixir', description: 'Type of the service (elixir/conch)'}),
+    type: Flags.string({char: 't', default: 'elixir', description: 'Type of the service (elixir/conch)'}),
     up: Flags.boolean({char: 'u', default: false, description: 'Run docker-compose up after building'}),
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Docker)
 
-    if(flags.up){
+    if (flags.up) {
       exec(`docker compose -f ${flags.file} up -d`, (error, stdout, stderr) => {
         if (error) {
-          console.error(`exec error: ${error}`);
-          return;
+          console.error(`exec error: ${error}`)
+          return
         }
 
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-      });
+        console.log(`stdout: ${stdout}`)
+        console.error(`stderr: ${stderr}`)
+      })
       return
     }
 
-    if(flags.down){
+    if (flags.down) {
       exec(`docker compose -f ${flags.file} down`, (error, stdout, stderr) => {
         if (error) {
-          console.error(`exec error: ${error}`);
-          return;
+          console.error(`exec error: ${error}`)
+          return
         }
 
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-      });
+        console.log(`stdout: ${stdout}`)
+        console.error(`stderr: ${stderr}`)
+      })
       return
     }
 
-    if(!flags.name && (!flags.up || !flags.down)){
-      console.log("Please provide a name for the container to build")
+    if (args.path !== 'bootstrap' && !flags.name && !flags.up && !flags.down) {
+      console.log('Please provide a name for the container to build')
       this.exit(1)
     }
 
-    // cd to path, docker build tag with name
-    const spinner = ora('Running docker build ..').start();
-    exec(`cd ${args.path}/${flags.type} && docker build -t ${flags.name} . > /dev/null 2>&1`, (error, stdout, stderr) => {
-      if (error) {
-        spinner.fail('Docker build failed');
-        console.error(`exec error: ${error}`);
-        return;
+    if (args.path === 'bootstrap') {
+      // if flags.file does not end with bootstrap.py then exit
+      if (!flags.file.endsWith('bootstrap.py')) {
+        console.log('Please provide a valid path to bootstrap.py file')
+        this.exit(1)
       }
+      // copy -f to container:/app/app/ and only restart after copy completes
+      exec(`docker cp ${flags.file} ${flags.container}:/app/app/bootstrap.py`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`)
+          return
+        }
 
-      spinner.succeed('Docker build successful');
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    });
+        console.log(`stdout: ${stdout}`)
+        console.error(`stderr: ${stderr}`)
+
+        // restart the container only after copy completes
+        exec(`docker restart ${flags.container}`, (restartError, restartStdout, restartStderr) => {
+          if (restartError) {
+            console.error(`exec error: ${restartError}`)
+            return
+          }
+
+          console.log(`stdout: ${restartStdout}`)
+          console.error(`stderr: ${restartStderr}`)
+        })
+      })
+      return
+    }
+    // cd to path, docker build tag with name
+    const spinner = ora('Running docker build ..').start()
+    exec(
+      `cd ${args.path}/${flags.type} && docker build -t ${flags.name} . > /dev/null 2>&1`,
+      (error, stdout, stderr) => {
+        if (error) {
+          spinner.fail('Docker build failed')
+          console.error(`exec error: ${error}`)
+          return
+        }
+
+        spinner.succeed('Docker build successful')
+        console.log(`stdout: ${stdout}`)
+        console.error(`stderr: ${stderr}`)
+      },
+    )
 
     // read the docker-compose file
-    const dockerCompose: any = yaml.load(fs.readFileSync(flags.file, 'utf8'));
+    if (!fs.existsSync(flags.file)) {
+      console.error(`Error: The file "${flags.file}" does not exist.`)
+      this.exit(1)
+    }
+    const dockerCompose: any = yaml.load(fs.readFileSync(flags.file, 'utf8'))
     // if type is elixir set image of backend to name, else set image of frontend to name
-    if(flags.type === 'elixir'){
+    if (flags.type === 'elixir') {
       dockerCompose.services.langserve.image = flags.name
-      dockerCompose.services.langserve.pull_policy = "if_not_present"
-    }else{
+      dockerCompose.services.langserve.pull_policy = 'if_not_present'
+    } else {
       dockerCompose.services.frontend.image = flags.name
-      dockerCompose.services.frontend.pull_policy = "if_not_present"
+      dockerCompose.services.frontend.pull_policy = 'if_not_present'
     }
 
     // write the docker-compose file
-    fs.writeFileSync(flags.file, yaml.dump(dockerCompose));
+    fs.writeFileSync(flags.file, yaml.dump(dockerCompose))
   }
 }
