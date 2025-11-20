9dc915d77fe5ca24b65a0fd8681b4a9aa2cab6f3
dermatologist <github@gulfdoctor.net>
2025-11-20 06:44:25 -0600

    fix: update demo script reference in E2E test and add missing dry-run command

--- e2e.sh
+++ e2e.sh
@@ -7,3 +7,4 @@ dhti-cli docker -n beapen/genai-test:1.0 -t elixir --dry-run
 dhti-cli conch install -g https://github.com/dermatologist/openmrs-esm-dhti-template.git -n openmrs-esm-dhti-template
 dhti-cli docker -n beapen/conch-test:1.0 -t conch --dry-run
 dhti-cli docker -u --dry-run
+dhti-cli docker -d --dry-run
