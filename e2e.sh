4f6e6fb3cbdf215c1377549d92432adadddf6fef
dermatologist <github@gulfdoctor.net>
2025-11-20 06:38:24 -0600

    feat: enhance demo script and add e2e script for streamlined setup

---
+++ e2e.sh
@@ -0,0 +1,9 @@
+rm -rf ~/dhti
+npm run build
+npm link
+dhti-cli compose add -m openmrs -m langserve
+dhti-cli elixir install -g https://github.com/dermatologist/dhti-elixir-template.git -n dhti-elixir-template
+dhti-cli docker -n beapen/genai-test:1.0 -t elixir --dry-run
+dhti-cli conch install -g https://github.com/dermatologist/openmrs-esm-dhti-template.git -n openmrs-esm-dhti-template
+dhti-cli docker -n beapen/conch-test:1.0 -t conch --dry-run
+dhti-cli docker -u --dry-run
