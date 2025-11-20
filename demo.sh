15d64f52bbc36cd9bc37c2d628ee0b803fae04a8
dermatologist <github@gulfdoctor.net>
2025-11-20 06:49:29 -0600

    fix: update demo script to use npx for command execution

--- demo.sh
+++ demo.sh
@@ -1,13 +1,11 @@
 rm -rf ~/dhti
-npm run build
-npm link
-dhti-cli compose add -m openmrs -m langserve
-dhti-cli elixir install -g https://github.com/dermatologist/dhti-elixir-template.git -n dhti-elixir-template
-dhti-cli docker -n beapen/genai-test:1.0 -t elixir
-dhti-cli conch install -g https://github.com/dermatologist/openmrs-esm-dhti-template.git -n openmrs-esm-dhti-template
-dhti-cli docker -n beapen/conch-test:1.0 -t conch
-dhti-cli docker -u
-## Waiit for all containers to be up and running before accessing OpenMRS
+npx -y dhti-cli compose add -m openmrs -m langserve
+npx -y dhti-cli elixir install -g https://github.com/dermatologist/dhti-elixir-template.git -n dhti-elixir-template
+npx -y dhti-cli docker -n beapen/genai-test:1.0 -t elixir
+npx -y dhti-cli conch install -g https://github.com/dermatologist/openmrs-esm-dhti-template.git -n openmrs-esm-dhti-template
+npx -y dhti-cli docker -n beapen/conch-test:1.0 -t conch
+npx -y dhti-cli docker -u
+## Wait for all containers to be up and running before accessing OpenMRS
 sleep 45
 HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost/openmrs/spa/home || true)
 TRIES=0
