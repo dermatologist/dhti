fc2c7f4126b2767795c3da09a85b3255d76791d4
dermatologist <github@gulfdoctor.net>
2026-01-23 11:32:03 -0600

    feat: add new demo scripts for docktor and packages

--- demos/packages.sh
+++ demos/packages.sh
@@ -4,7 +4,7 @@ dhti-cli compose add -m langserve -m redis
 dhti-cli elixir install -g https://github.com/dermatologist/dhti-elixir.git -b debug -n dhti-elixir-schat -s packages/simple_chat
 dhti-cli elixir install -g https://github.com/dermatologist/dhti-elixir.git -b debug -n dhti-elixir-upload -s packages/upload_file
 dhti-cli elixir install -g https://github.com/dermatologist/dhti-elixir.git -b debug -n dhti-elixir-srag -s packages/simple_rag
-dhti-cli docker -n yourdockerhandle/genai-test:3.0 -t elixir
+dhti-cli docker -n yourdockerhandle/genai-test:1.0 -t elixir
 dhti-cli docker -u
 dhti-cli conch install -g dermatologist/openmrs-esm-dhti -s packages/esm-chatbot-agent -n esm-chatbot-agent
 # dhti-cli conch start -n esm-chatbot-agent -w /home/beapen/dhti -s packages/esm-chatbot-agent -s packages/esm-generic-display
