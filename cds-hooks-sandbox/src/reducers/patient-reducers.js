3da5a19017e5e02530d0a890ee97997c73c806de
SANGHATI23 <your_personal_email@gmail.com>
2026-04-15 14:17:07 -0500

    Update default sandbox patient to valid HAPI R4 patient

---
+++ cds-hooks-sandbox/src/reducers/patient-reducers.js
@@ -0,0 +1,78 @@
+import * as types from '../actions/action-types';
+
+const initialState = {
+  defaultPatientId: '90263196',
+  defaultUser: 'Practitioner/COREPRACTITIONER1',
+  currentUser: '',
+  currentPatient: {
+    id: '90263196',
+    name: 'Karla',
+    birthDate: '1980-01-02',
+    patientResource: {},
+    conditionsResources: [],
+  },
+};
+
+const patientReducers = (state = initialState, action) => {
+  if (action.type) {
+    switch (action.type) {
+      // Store Patient resource from successful connection to patient in context from FHIR server
+      case types.GET_PATIENT_SUCCESS: {
+        const { patient } = action;
+        let fullName = 'Unknown';
+        if (Array.isArray(patient.name)) {
+          const familyName = (Array.isArray(patient.name[0].family)) ? patient.name[0].family.join(' ') : patient.name[0].family;
+          fullName = `${patient.name[0].given.join(' ')} ${familyName}`;
+        }
+        const newPatient = {
+          id: patient.id,
+          name: fullName,
+          birthDate: patient.birthDate,
+          patientResource: patient,
+        };
+
+        let filteredEntries = [];
+        if (action.conditions && action.conditions.total > 0) {
+          const conditionCodes = [];
+          filteredEntries = action.conditions.entry.filter((item) => {
+            const { resource } = item;
+            const hasAppropriateCode = resource && resource.code && resource.code.coding
+              && resource.code.coding[0] && resource.code.coding[0].code;
+            if (hasAppropriateCode) {
+              const isDuplicate = conditionCodes.indexOf(resource.code.coding[0].code);
+              if (isDuplicate < 0) {
+                conditionCodes.push(resource.code.coding[0].code);
+                return true;
+              }
+              return false;
+            }
+            return false;
+          });
+        }
+        filteredEntries = filteredEntries.sort((a, b) => {
+          if (a.resource.code.text < b.resource.code.text) { return -1; }
+          if (b.resource.code.text < a.resource.code.text) { return 1; }
+          return 0;
+        });
+        newPatient.conditionsResources = filteredEntries;
+        return { ...state, currentPatient: newPatient };
+      }
+
+      // Stores the current user ID in store if specified in an access token returned to the Sandbox (on SMART launched Sandbox)
+      case types.SMART_AUTH_SUCCESS: {
+        const { authResponse } = action;
+        if (authResponse && authResponse.userId) {
+          return { ...state, currentUser: authResponse.userId };
+        }
+        return state;
+      }
+
+      default: {
+        return state;
+      }
+    }
+  }
+  return state;
+};
+
+export default patientReducers;
---
+++ cds-hooks-sandbox/tests/reducers/patient-reducers.test.js
@@ -0,0 +1,127 @@
+import reducer from '../../src/reducers/patient-reducers';
+import * as types from '../../src/actions/action-types';
+
+describe('Patient Reducers', () => {
+  let state = {};
+
+  beforeEach(() => {
+    state = {
+      defaultPatientId: '90263196',
+      defaultUser: 'Practitioner/COREPRACTITIONER1',
+      currentUser: '',
+      currentPatient: {
+        id: '90263196',
+        name: 'Karla',
+        birthDate: '1980-01-02',
+        patientResource: {},
+        conditionsResources: [],
+      },
+    };
+  });
+
+  it('returns the initial state without action', () => {
+    expect(reducer(undefined, {})).toEqual(state);
+  });
+
+  describe('GET_PATIENT_SUCCESS', () => {
+    let patient = {
+      id: 'patient-example-id',
+      birthDate: '2017-12-17',
+      name: [
+        {
+          given: ['Given'],
+          family: 'Family No Array',
+        },
+      ],
+    };
+    let action = {
+      type: types.GET_PATIENT_SUCCESS,
+      patient,
+    };
+
+    it('handles the action with family name not in an array', () => {
+      const fullName = `${action.patient.name[0].given.join(' ')} ${action.patient.name[0].family}`;
+      const newPatient = {
+        id: action.patient.id,
+        name: fullName,
+        birthDate: action.patient.birthDate,
+        patientResource: action.patient,
+        conditionsResources: [],
+      };
+
+      const newState = Object.assign({}, state, {
+        currentPatient: newPatient,
+      });
+
+      expect(reducer(state, action)).toEqual(newState);
+    });
+
+    it('handles the action with family in an array', () => {
+      patient = Object.assign({}, patient, {
+        name: [
+          {
+            given: ['Given'],
+            family: ['Family', 'Array'],
+          },
+        ],
+      });
+      action = Object.assign({}, action, {
+        patient,
+      });
+
+      const fullName = `${action.patient.name[0].given.join(' ')} ${action.patient.name[0].family.join(' ')}`;
+      const newPatient = {
+        id: action.patient.id,
+        name: fullName,
+        birthDate: action.patient.birthDate,
+        patientResource: action.patient,
+        conditionsResources: [],
+      };
+
+      const newState = Object.assign({}, state, {
+        currentPatient: newPatient,
+      });
+
+      expect(reducer(state, action)).toEqual(newState);
+    });
+  });
+
+  describe('SMART_AUTH_SUCCESS', () => {
+    it('updates the current user if a user is supplied in the SMART auth response', () => {
+      const authResponseMock = {
+        userId: 'Practitioner/some-user',
+      };
+      const metadataMock = { fhirVersion: '1.0.2' };
+      const action = {
+        type: types.SMART_AUTH_SUCCESS,
+        authResponse: authResponseMock,
+        metadata: metadataMock,
+      };
+
+      const newState = Object.assign({}, state, {
+        currentUser: authResponseMock.userId,
+      });
+
+      expect(reducer(state, action)).toEqual(newState);
+    });
+
+    it('returns state if a user is not supplied in the SMART auth response', () => {
+      const authResponseMock = {};
+      const metadataMock = { fhirVersion: '1.0.2' };
+      const action = {
+        type: types.SMART_AUTH_SUCCESS,
+        authResponse: authResponseMock,
+        metadata: metadataMock,
+      };
+
+      expect(reducer(state, action)).toEqual(state);
+    });
+  });
+
+  describe('Pass-through Actions', () => {
+    it('should return state if an action should pass through this reducer without change to state', () => {
+      const action = { type: 'SOME_OTHER_ACTION' };
+      expect(reducer(state, action)).toEqual(state);
+    });
+  });
+});
